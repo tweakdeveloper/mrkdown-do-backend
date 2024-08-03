43f3a2800fa30aef5d1d48f39a7e99cce740fc7f
Nicola Clark <nicola@slottedspoon.dev>
2024-08-03 05:15:28 -0500

    now requires state param from client

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const app = express();
 
 app.get('/', (_req, res) => res.send('howdy!!! 🤠'));
 
-app.get('/init_auth_flow', (_req, res) => {
+app.get('/init_auth_flow', (req, res) => {
+  if (!req.query.state) {
+    res.status(400).send('needs state query parameter');
+  }
   let authURL = new URL('https://www.tumblr.com/oauth2/authorize');
   authURL.searchParams.append('client_id', process.env.TUMBLR_CLIENT_ID);
   authURL.searchParams.append('response_type', 'code');
   authURL.searchParams.append('scope', '');
-  authURL.searchParams.append('state', crypto.randomUUID());
+  authURL.searchParams.append('state', req.query.state);
   authURL.searchParams.append(
     'redirect_uri',
     'mrkdown://mrkdown.slottedspoon.dev/auth/redirect',
